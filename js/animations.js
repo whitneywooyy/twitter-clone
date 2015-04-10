8bd11fccb667c680c0e43efda2c3a8d413cb230e
Whitney <whitneywooyy@gmail.com>
2015-04-10 16:24:31 -0600

    Working on 5th part

--- js/animations.js
+++ js/animations.js
@@ -1,21 +1,22 @@
 $(document).ready(function(){
 
-	// var counter = 0;
-	// var maxChars = $("#char-count");
-
 	$("#tweet-content .tweet-compose").focus(function(){
 		$(this).addClass("addHeight");
 		$("#tweet-controls").css({'display': 'block'}); 
 	})	// end #tweet-content .tweet-compose focus fn
 	$("#tweet-content .tweet-compose").blur(function(){
-		$(this).removeClass("addHeight");
-		$("#tweet-controls").css({'display': 'none'}); 
+		if (!$(this).val()) {
+			$(this).removeClass("addHeight");
+			$("#tweet-controls").css({'display': 'none'});
+		}
 	})	// end #tweet-content .tweet-compose blur fn 	// TO DO >> Add if statement saying, if there are characters typed in the textarea (.tweet-compose), then make it so that blur() cannot happen.
 	$(".reply .tweet-compose").focus(function(){
 		$(this).addClass("addHeight");
 	})	// end .reply .tweet-compose focus fn
 	$(".reply .tweet-compose").blur(function(){
-		$(this).removeClass("addHeight");
+		if (!$(this).val()) {
+			$(this).removeClass("addHeight");
+		}
 	})	// end .reply .tweet-compose blur fn 	// TO DO >> Add if statement saying, if there are characters typed in the textarea (.tweet-compose), then make it so that blur() cannot happen.
 	$(".tweet-compose").on("keypress keyup", function(){
 		var charsRemaining = 140 - ($(".tweet-compose").val().length);
@@ -36,7 +37,13 @@ $(document).ready(function(){
 			$(".button").prop("disabled", false);
 		}
 	};
-
+	$("#tweet-submit").on("click", function(){
+		var newTweet = $("#tweet-content .tweet-compose").val();
+		console.log("Message " + newTweet);
+		$("#stream").prepend($(".tweet"));
+		//$(".tweet-text").prepend($("#tweet-content .tweet-compose").val());
+	})
+		
 
 
 
